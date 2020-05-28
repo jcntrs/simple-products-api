ee5eceeb9ae1abeca1aada69a4aa20f118aafd58
Jean C. Contreras <jean.cntrs@gmail.com>
2020-05-28 00:48:43 -0400

    Was added settings for deploy in heroku

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ app.get('/', (req, res) => {
     res.send('Hello World - Simple Product API');
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Servidor funcionando en puerto ${port}`)
 });
